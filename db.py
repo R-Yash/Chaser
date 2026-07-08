71f0631cefcfcb031908ed893b6c631c05797356
Raja Yashwanth Avantsa <yashwanth5836@gmail.com>
2026-07-08 23:28:11 +0530

    Arg fix

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from sqlmodel import SQLModel, create_engine, Session
 load_dotenv()
 
-engine = create_engine(os.getenv("DATABASE_URL"), connect_args={"check_same_thread": False} )
+engine = create_engine(os.getenv("DATABASE_URL"))
 
 def init_db():
     SQLModel.metadata.create_all(engine)
